ee58e858948e4586b1178ea977c34d863036f06f
lkrasovit <lkrasovit@gmail.com>
2018-12-02 21:03:44 +0200

    nav+home+rauting

---
+++ app/app.js
@@ -0,0 +1,22 @@
+var app = angular.module("homeApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "app/home/home.html"
+    }).when("/login", {
+        templateUrl: "app/login/login.html",
+        controller: "loginCtrl"
+
+    }).when("/signup", {
+
+    // }).when("/messages", {
+    //     templateUrl: "app/messages/msgList.html",
+    //     controller: "msgListCtrl"
+    // }).when("/new" , {
+    //     templateUrl: "app/messages/newMsg.html",
+    //     controller: "newMsgCtrl"
+    // }).when("/recipe/:id" , {
+
+    // })
+})
