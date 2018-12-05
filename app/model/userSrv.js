6b163cb675fbbdb713026f48f18c889f57a59d3e
lkrasovit <lkrasovit@gmail.com>
2018-12-05 10:03:02 +0200

    json-server

--- app/model/userSrv.js
+++ app/model/userSrv.js
@@ -9,7 +9,7 @@ app.factory("user", function($q,$http) {
    }
     function login(email, pwd) {
        var async = $q.defer();
-       var loginURL = "http://my-json-server.typicode.com/nirch/recipe-book-v3/users?email=" +
+       var loginURL = "http://my-json-server.typicode.com/lkrasovit/HAMS/users?email=" +
        email + "&pwd=" + pwd;
    $http.get(loginURL).then(function(response) {
        if (response.data.length > 0) {
