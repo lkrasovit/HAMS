18da4c86d27be1d6330fd4c14355d0a08a13222f
lkrasovit <lkrasovit@gmail.com>
2018-12-07 22:30:06 +0200

    bug fix missing $location + redirect to home page for non-loggedin users

--- app/recipes/newRecipeCtrl.js
+++ app/recipes/newRecipeCtrl.js
@@ -1,12 +1,18 @@
-app.controller("newRecipeCtrl", function($scope, recipes) {
-    
-    
-    $scope.createRecipe = function () {
-        recipes.createRecipe($scope.name, $scope.description, 
-            $scope.ingrediants, $scope.steps,  $scope.image).then(function () {
-            $location.path("/recipes")
-        }, function (err) {
-            console.log(err);
-        })
+app.controller("newRecipeCtrl", function ($scope, recipes, $location, user) {
+
+    // Checking if the user is currently logged in,
+    // if not redirecting to the home page
+    if (!user.isLoggedIn()) {
+        $location.path("/");
+        return;
     }
- }) 
+
+    $scope.createRecipe = function () {
+        recipes.createRecipe($scope.name, $scope.description,
+            $scope.ingrediants, $scope.steps, $scope.image).then(function () {
+                $location.path("/recipes")
+            }, function (err) {
+                console.log(err);
+            })
+    }
+}) 
