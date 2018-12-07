b979572bf446b69440eadd64fb641f5db7b83b83
lkrasovit <lkrasovit@gmail.com>
2018-12-07 23:13:37 +0200

    Adding support for multiple users in recipe service

--- app/model/recipesSrv.js
+++ app/model/recipesSrv.js
@@ -1,7 +1,8 @@
-app.factory("recipes", function($q, $http, user) {
- 
-    var recipes = [];
-    var wasEverLoaded = false;
+app.factory("recipes", function ($q, $http, user) {
+
+
+    var recipes = {};
+    var wasEverLoaded = {};
 
     function Recipe(plainRecipe) {
         this.id = plainRecipe.id;
@@ -13,62 +14,53 @@ app.factory("recipes", function($q, $http, user) {
         this.userId = plainRecipe.userId;
     }
 
-
-
-
     function getActiveUserRecipes() {
         var async = $q.defer();
+        var userId = user.getActiveUser().id;
 
-        // recipes = [];
-        
-        // var getRecipesURL = "http://my-json-server.typicode.com/nirch/recipe-book-v3/recipes?userId=" +
-        //     user.getActiveUser().id;
-        
-        // $http.get(getRecipesURL).then(function(response) {
-        //     for (var i = 0; i < response.data.length; i++) {
-        //         var recipe = new Recipe(response.data[i]);
-        //         recipes.push(recipe);
-        //     } 
-        // This is a hack since we don't really have a persistant server.
-        // So I want to get all recipes only once.
-        if (wasEverLoaded) {           
+
+        if (wasEverLoaded) {
             async.resolve(recipes);
-        // }, function(error) {
-        //     async.reject(error);
-        // })
-    } else {
-        var getRecipesURL = "http://my-json-server.typicode.com/nirch/recipe-book-v3/recipes?userId=" +
-            user.getActiveUser().id;
-        
-        $http.get(getRecipesURL).then(function(response) {
-            for (var i = 0; i < response.data.length; i++) {
-                var recipe = new Recipe(response.data[i]);
-                recipes.push(recipe);
+
+            if (wasEverLoaded[userId]) {
+                async.resolve(recipes[userId]);
+            } else {
+
+                recipes[userId] = [];
+                var getRecipesURL = "http://my-json-server.typicode.com/lkrasovit/HAMS/recipes?userId=" + userId;
+
+                $http.get(getRecipesURL).then(function (response) {
+                    for (var i = 0; i < response.data.length; i++) {
+                        var recipe = new Recipe(response.data[i]);
+                        recipes[userId].push(recipe);
+                    }
+                    wasEverLoaded[userId] = true;
+                    async.resolve(recipes[userId]);
+                }, function (error) {
+                    async.reject(error);
+                });
             }
-            wasEverLoaded = true;
-            async.resolve(recipes);
-        }, function(error) {
-            async.reject(error);
-        });
-    }
 
-       return async.promise;
-   }
-    function createRecipe(name, description, ingredients, steps, imgUrl) {
-        
-       var async = $q.defer();
-        var newRecipe = new Recipe({id:-1, name: name, description: description,
-           ingredients: ingredients, steps: steps, imgUrl: imgUrl, 
-           userId: user.getActiveUser().id});
-        // if working with real server:
-       //$http.post("http://my-json-server.typicode.com/nirch/recipe-book-v3/recipes", newRecipe).then.....
-        recipes.push(newRecipe);
-       async.resolve(newRecipe);
-        return async.promise;
-   }
-    return {
-      
-       getActiveUserRecipes: getActiveUserRecipes,
-       createRecipe: createRecipe
-   }
-}) 
+            return async.promise;
+        }
+    }
+function createRecipe(name, description, ingredients, steps, imgUrl) {
+
+    var async = $q.defer();
+    var userId = user.getActiveUser().id;
+    var newRecipe = new Recipe({
+        id: -1, name: name, description: description,
+        ingredients: ingredients, steps: steps, imgUrl: imgUrl,
+        userId: userId
+    });
+
+    recipes[userId].push(newRecipe);
+    async.resolve(newRecipe);
+    return async.promise;
+}
+return {
+
+    getActiveUserRecipes: getActiveUserRecipes,
+    createRecipe: createRecipe
+}
+    }) 
