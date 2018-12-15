2d91625a42a9cef3a84a989de1cd6e1165b146f7
lkrasovit <lkrasovit@gmail.com>
2018-12-15 17:01:13 +0200

    ups(((

--- app/recipes/newRecipeCtrl.js
+++ app/recipes/newRecipeCtrl.js
@@ -6,7 +6,12 @@ app.controller("newRecipeCtrl", function ($scope, recipes, $location, user) {
         $location.path("/");
         return;
     }
-
+// // Checking if the user is currently logged in,
+// //     if not redirecting to the home page
+//     if (!user.isLoggedIn()) {
+//         $location.path("/");
+//         return;
+//     }
     $scope.createRecipe = function () {
         recipes.createRecipe($scope.name, $scope.description,
             $scope.ingrediants, $scope.steps, $scope.image).then(function () {
--- app/recipes/recipeHomeCtrl.js
+++ app/recipes/recipeHomeCtrl.js
@@ -1,26 +1,26 @@
-app.controller("recipeHomeCtrl", function($scope, recipes, user, $location) {
+app.controller("recipeHomeCtrl", function ($scope, recipes, user, $location) {
     // Checking if the user is currently logged in,
-   // if not redirecting to the home page
-   if (!user.isLoggedIn()) {
-       $location.path("/");
-       return;
-   }
-    
-    
+    // if not redirecting to the home page
+    if (!user.isLoggedIn()) {
+        $location.path("/");
+        return;
+    }
+
+
+
     recipes.getActiveUserRecipes().then(function (recipes) {
         $scope.recipes = recipes;
-    }, function(error) {
-        
+    }, function (error) {
+
     })
-    $scope.deleteRow = function(index ){
+    $scope.deleteRow = function (index) {
         $scope.recipes.splice(index, 1);
-      }
+    }
 
     //   $scope.name = "",
-      $scope.clicked = false,
-      $scope.click = function click(index){
-        $scope.clicked = !$scope.clicked;
-      }
- }) 
+    $scope.clicked = false,
+        $scope.click = function click(index) {
+            $scope.clicked = !$scope.clicked;
+        }
+})
 
- 
